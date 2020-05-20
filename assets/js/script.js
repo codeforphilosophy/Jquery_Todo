9d92b0a1556d07a299ee6a43276d1b034538ccdb
codeforphilosophy <sariyya.abdullayeva@gmail.com>
2020-05-20 13:54:23 +0300

    add: logic delete task

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,25 @@
-
 $( document ).ready(function() {
+    //Logic starts here
+
+    //STRICKTHROUGH DONE 
     $("li").click(function(){
         $(this).toggleClass("done");
       });
+    
+    //DELETE THE TASK. NOTE!!!! THIS IS AN EVENT BOOBLING SITUATION!!!-TO STOR 1. ADD 'e' TO FUNCTION, THEN 2. INSIDE THE FUNCTION ADD e.stopPropagation,
+    $('span').click(function(e){
+        $(this).parent().fadeOut(500, function(){
+            $(this).remove();
+        });
+        e.stopPropagation();
+    });
+
+
+
+
+
+
+
+
+
 });
